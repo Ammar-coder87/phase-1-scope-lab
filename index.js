586e4984121ea4776baeffbbf9910e72d3d2114f
Ammar-coder87 <ammar.kolko87@gmail.com>
2023-10-11 02:32:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+customerName = customerName.toUpperCase();
+return console.log(customerName);
+}
+//upperCaseCustomerName();
+
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    
+    return console.log(bestCustomer);
+}
+
+//setBestCustomer();
+//var bestCustomer = "not bob";
+
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return console.log(bestCustomer);
+}
+//overwriteBestCustomer();
+
+const leastFavoriteCustomer = "initValue";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "nextValue";
+    return leastFavoriteCustomer;
+}
+//console.log(changeLeastFavoriteCustomer());
